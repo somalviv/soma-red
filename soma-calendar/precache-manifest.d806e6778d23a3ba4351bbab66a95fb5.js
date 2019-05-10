74dc449bf7facc995def04e396f0b9074c7d4b69
utoyvo <utoyvo@gmail.com>
2019-05-10 17:32:57 +0300

    #1

---
+++ soma-calendar/precache-manifest.d806e6778d23a3ba4351bbab66a95fb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c91cee8b2aebf8d03d",
+    "url": "/soma-calendar/static/css/main.83d8e9f1.chunk.css"
+  },
+  {
+    "revision": "35c91cee8b2aebf8d03d",
+    "url": "/soma-calendar/static/js/main.1d9591bb.chunk.js"
+  },
+  {
+    "revision": "4df494f2c53a4379ec77",
+    "url": "/soma-calendar/static/js/runtime~main.16fc1238.js"
+  },
+  {
+    "revision": "dcc697a90c05db20f180",
+    "url": "/soma-calendar/static/js/2.28b78aa5.chunk.js"
+  },
+  {
+    "revision": "fc14f8b53de2f65d92032e182853a38f",
+    "url": "/soma-calendar/index.html"
+  }
+];
